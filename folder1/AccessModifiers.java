e7982fd25dfaf8c7f04b5394644d86a502985a93
VikramLekkala <97777772+VikramLekkala@users.noreply.github.com>
2022-01-16 22:47:54 +0530

    Add files via upload

---
+++ folder1/AccessModifiers.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+public class AccessModifiers {
+public static void main(String[] args) {
+	child1 obj1=new child1();
+	obj1.display();
+	Class1 obj2=new Class1();
+	obj2.display();
+}
+}
---
+++ folder1/Class1.java
@@ -0,0 +1,12 @@
+package pkg1;
+
+public class Class1 {
+	 void display() {
+		 Parent p=new Parent();
+		// p.method();--private method
+		 p.method1();
+		 p.method2();
+		 p.method3();
+		 }
+}
+
