9690920da6ffec9b757232eb5b50dab85da8d060
VikramLekkala <97777772+VikramLekkala@users.noreply.github.com>
2022-01-16 22:46:21 +0530

    Add files via upload

---
+++ folder/TypeCasting.java
@@ -0,0 +1,18 @@
+package PracticeProject1;
+
+
+	import java.util.Scanner;
+
+	public class TypeCasting {
+		public static void main(String[] args) {
+	String s1="100";
+	String s2="200";
+	int add=Integer.parseInt(s1)+Integer.parseInt(s2);
+		System.out.println(add);
+		double add1=Double.parseDouble(s1)+Double.parseDouble(s2);
+		System.out.println(add1);
+	}
+		}
+
+
+
