a334a4851ecd1c91080cea6bac6e6e16662bafd5
VikramLekkala <97777772+VikramLekkala@users.noreply.github.com>
2022-01-19 16:26:23 +0530

    Add files via upload

---
+++ folder/FinallyBlockDemo.java
@@ -0,0 +1,18 @@
+package MultiThreading;
+
+public class FinallyBlockDemo {
+
+	public static void main(String[] args) {
+		int a=45,b=0,res=0;
+		try {
+			res=a/b;
+		}catch(ArithmeticException e) {
+			System.out.println("Error "+e.getMessage());
+		}
+		finally {
+			System.out.println("The result is:"+res);
+		}
+
+	}
+
+}
