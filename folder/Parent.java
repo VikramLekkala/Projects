87e07a7876103650f2f6c4d85e23e9c1d244fa72
VikramLekkala <97777772+VikramLekkala@users.noreply.github.com>
2022-01-27 20:28:22 +0530

    Add files via upload

---
+++ folder/Parent.java
@@ -0,0 +1,19 @@
+package pkg1;
+
+
+public class Parent {
+	 
+			private void method() {
+				System.out.println("it is the private method");
+			}
+			void method1() {
+				System.out.println("it is the default method");
+			}
+			protected void method2() {
+				System.out.println("it is the protected method");
+			}
+			public void method3() {
+				System.out.println("it is the public method");
+			}
+}
+
---
+++ folder/child1.java
@@ -0,0 +1,11 @@
+package pkg1;
+
+public class child1 extends Parent {
+	void display() {
+		//method();---private method is used within the class only
+	     method1();
+		method2();
+		method3();
+	 }
+}
+
